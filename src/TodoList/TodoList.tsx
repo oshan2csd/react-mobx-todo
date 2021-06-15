64b1ed914de48e7aa1d5353a8f34f79c6bd436ee
oshan2csd <oshan2csd@gmail.com>
2021-06-15 23:49:11 +1000

    responsive design added with react-bootstrap

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import  React, { useState } from 'react'
-import { Card } from 'react-bootstrap';
+import { Button, Card, Form, FormGroup, FormLabel } from 'react-bootstrap';
 import TodoStore from './TodoStore';
 
 
@@ -16,71 +16,75 @@ function TodoList({todoStore}: TodoListProps) {
     //access it as a property. 
 
     
-    function handleSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, value: string) { 
-        if(value){
+    function handleSubmit(event: React.MouseEvent<HTMLElement, MouseEvent>, value: string) { 
+        if(value && value.trim().length > 0){
+            event.preventDefault();
             todoStore.addTodo(value);
             setItem('');
+        }
+        else{
+            alert('Please enter a value..!');
+            event.preventDefault();
         }
     }
     
     return(
         <>
-            <h1>Simple ToDo List</h1>
-            <h5>
-                <p>
-                    <i style={{color:'red'}} >
-                        Note: This project has used, <a href='https://reactjs.org/'>React</a> and <a href='https://mobx.js.org/README.html'>MobX </a>for state management
-                        <br/>
-                        <br/> 
-                    </i>                    
-                </p>
-                <Card>
-                    <Card.Body>
-                         Type a todo and click on submit<br/>
-                         When you completed an item click on that item to mark completed<br/>
-                         Click again to mark uncompleted
-                    </Card.Body>
-                </Card>
-            </h5>
-            <input 
-                type='text' 
-                onChange={ (event) => {
-                    setItem(event.target.value);
-                    }
-                }
-                value={item}
-            />
-
-            <button onClick= { (event) => {
-                    handleSubmit(event, item);                   
-                }} 
-            >
-                Submit
-            </button>
-
-            <br/>
-            Completed: {status.completed}
-            <br/>
-            Remaining: {status.remaining}
-            
-
-{/*
-* As soon as submit clicked, todos list will change (**Note: todos is a property of Store)
-* By wrapping component inside a observer Higher Order Function,
-* It will notify TodoList Component about state change
-* So, it will re-render as soon as something changed 
-*/}
-            <ul>
-                {todoStore.todos.map( (t) => {
-                    return <div key={t.id}>
-                                <input type='Checkbox' checked={t.isCompleted} onChange= {() => {
-                                    todoStore.toggleTodo(t.id)}} >
-                                </input>   
-                                {t.title}    
-                            </div>
-                })}
-            </ul>
-            
+        <div className="App">
+            <header className="App-header">             
+                <Form style={{margin:'50px'}}>  
+                    <Form.Group controlId="formBasicInput">
+                        <Form.Label>Add todo now!</Form.Label>
+                        <Form.Control type="input" placeholder="Enter todo here..." onChange={ (event) => {
+                                        setItem(event.target.value);
+                                    }
+                            }
+                            value={item}>                        
+                        </Form.Control>
+                        <Form.Text className="text-muted">
+                            This will be saved locally!
+                        </Form.Text>                        
+                    </Form.Group>                    
+                    <Button  style={{margin:'15px'}} variant="primary" type="submit" onClick= { (event) => {
+                                                handleSubmit(event, item);}}>                    
+                            Add Todo                    
+                    </Button>{''}
+                    <Form.Group >
+                        <Form.Label>Completed: {status.completed}</Form.Label><br/>                   
+                        <Form.Label>Remaining: {status.remaining}</Form.Label>
+                    </Form.Group>
+                    
+                    {/*
+                    * As soon as submit clicked, todos list will change (**Note: todos is a property of Store)
+                    * By wrapping component inside a observer Higher Order Function,
+                    * It will notify TodoList Component about state change
+                    * So, it will re-render as soon as something changed 
+                    */}
+                    
+                    <ul>
+                        {todoStore.todos.map( (t) => {
+                            return <div key={t.id}>  
+                                        <FormGroup style={{display: 'flex'}}>                                  
+                                            <Form.Check  inline type="checkbox" checked={t.isCompleted} label={t.title} onChange= {() => {
+                                                todoStore.toggleTodo(t.id)}}  >
+                                            </Form.Check>                                        
+                                        </FormGroup>     
+                                    </div>
+                        })}
+                    </ul>                              
+                    <Card className="mb-3" style={{color:'black'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                Todozzz
+                        </Card.Title>
+                        <Card.Text>
+                            This project powered by <a href='https://reactjs.org/'>React</a> with <a href='https://mobx.js.org/README.html'> MobX</a> (library for state management) 
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>                            
+                </Form>
+            </header>
+        </div>            
         </>
     )
 }
